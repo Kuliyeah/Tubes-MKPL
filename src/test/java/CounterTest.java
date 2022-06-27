5c750c3c18701fe120360607107775602dde0ce3
dhanakbar <maulana261100@gmail.com>
2022-06-27 21:38:54 +0700

    Add file project counting

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -19,24 +19,4 @@ public class CounterTest {
 		assertEquals(testCounter.getCount(), 0);
 	}
 
-	@Test
-	public void testIncrement() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.increment();
-			assertEquals(testCounter.getCount(), i);
-		}
-	}
-
-	@Test
-	public void testDecrement() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * -1);
-		}
-	}
-
 }
