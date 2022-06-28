39e4110c2f85b7fa09cb56a555b9fe6a1b24c644
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2022-06-28 22:59:56 +0700

    Update FibonacciCounter.java

--- src/test/java/FibonacciCounter.java
+++ src/test/java/FibonacciCounter.java
@@ -7,9 +7,10 @@ import org.junit.Test;
 public class FibonacciCounter {
 
 	@Test
-	public void testReset() {
+	public void testFibonacci() {
 		Fibonacci testFibo = new Fibonacci();
 		assertEquals(testFibo.fibonacciCounter(9), "1 2 3 5 8 13 21 34 55 ");
+		assertEquals(testFibo.fibonacciCounter(10), "1 2 3 5 8 13 21 34 55 89 ");
 	}
 
 }
