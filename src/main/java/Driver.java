deca5a64fcd8a26803619746de33937aff7b30fd
dhanakbar <maulana261100@gmail.com>
2022-06-28 22:25:52 +0700

    Add code in drive

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,6 +5,8 @@ class Driver {
 	public static void main(String[] args) {
 		Fibonacci fibo = new Fibonacci();
     System.out.println(fibo.fibonacciCounter(9));
+    System.out.println(fibo.fibonacciCounter(10));
+    System.out.println(fibo.fibonacciCounter(40));
 	}
 
 }
