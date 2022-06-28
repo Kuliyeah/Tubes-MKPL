d1dbd0861c5c739a750dda3fa3892d3e17dbc71a
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-29 00:11:27 +0700

    Update FibonacciCounter.java

--- src/test/java/FibonacciCounter.java
+++ src/test/java/FibonacciCounter.java
@@ -7,8 +7,7 @@ import org.junit.Test;
 public class FibonacciCounter {
 
 	@Test
-	public void testFibonacci
-	{
+	public void testFibonacci() {
 		Fibonacci testFibo = new Fibonacci();
 		assertEquals(testFibo.fibonacciCounter(9), "1 2 3 5 8 13 21 34 55 ");
 		assertEquals(testFibo.fibonacciCounter(10), "1 2 3 5 8 13 21 34 55 89 ");
