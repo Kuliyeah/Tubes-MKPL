041a6bc0ced54573ba01145b05081dc35d60564e
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-28 23:01:37 +0700

    Update Fibonacci.java

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,25 +1,29 @@
 package src.main.java;
 
 public class Fibonacci {
-	public int i,j=0,k=0;
+	public int i, j = 0, k = 0;
 	public String hasil = "";
-	
-	public Fibonacci() {}
-	
-	public String fibonacciCounter(int input){
-		for (i=1;i<=input;i++) {
-			if (i%2==0) {
-					if (k==0) k=i;
-					else k=k+j;
-					hasil = hasil + String.valueOf(k) + " ";
+
+	public Fibonacci() []
+
+	public String fibonacciCounter(int input) {
+		for (i = 1; i <= input; i++) {
+			if (i % 2 == 0) {
+				if (k == 0)
+					k = i;
+				else
+					k = k + j;
+				hasil = hasil + String.valueOf(k) + " ";
 			} else {
-					if (j==0) j=i;
-					else j=k+j;
-					hasil = hasil + String.valueOf(j) + " ";
+				if (j == 0)
+					j = i;
+				else
+					j = k + j;
+				hasil = hasil + String.valueOf(j) + " ";
 			}
 		}
 
 		return hasil;
 	}
-	
+
 }
