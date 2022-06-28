cc29d4b5ba83943b413eb4af5d300cd916d7ff5c
Dzakwan Daffa Hidayatullah <dzakwandh9@gmail.com>
2022-06-28 22:48:07 +0700

    coba tambah

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,6 +4,7 @@ class Driver {
 
 	public static void main(String[] args) {
 		Fibonacci fibo = new Fibonacci();
+    System.out.println("Contoh");
     System.out.println(fibo.fibonacciCounter(9));
     System.out.println(fibo.fibonacciCounter(10));
     System.out.println(fibo.fibonacciCounter(40));
